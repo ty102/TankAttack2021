7fcef78ee3da4af4c19e2e9f3c3a9dd8c793cbbc
Minhee Kang <ckrckr_@daum.net>
2021-04-27 10:53:40 +0900

    TankCtrl

--- Assets/02.Scripts/TankCtrl.cs
+++ Assets/02.Scripts/TankCtrl.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class TankCtrl : MonoBehaviour
 {
+    private Transform tr;
+    public float  speed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        tr = GetComponent<Transform>();
+        GetComponent<Rigidbody>().centerOfMass = new Vector3(0, -5.0f, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        tr.Translate (Vector3.forward* Time.deltaTime * speed * v);
+        tr.Rotate(Vector3.up * Time.deltaTime * 100.0f * h);
     }
 }
